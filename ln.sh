ffab20344b517ad77e04ac2f5017ecc605d59ba9
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2024-06-04 14:57:55 -0500

    chore: clean up code

--- ln.sh
+++ ln.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 filename="$1"
-# ncdump -h $filename > $filename.cdl
+ncdump -h $filename > $filename.cdl
 cd /scr/hyoklee/src/hyrax/bes/modules/hdf5_handler/data
-# ln -s /scr/hyoklee/data/${filename}
+ln -s /scr/hyoklee/data/${filename}
 cd /scr/hyoklee/src/hyrax/bes/modules/dmrpp_module/data
 ln -s /scr/hyoklee/data/${filename}
 cd /scr/hyoklee/src/hyrax/bes/modules/fileout_netcdf/data
