4a8f282063f3a359e8fb94f9fa583adeed6e5ca1
H. Joe Lee <hyoklee@hdfgroup.org>
2026-02-14 10:41:04 -0600

    test: omi l2 v4

--- dmrpp.sh
+++ dmrpp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Generate DMRPP.
+# Generate DMRPP. Run this after dmr.sh.
 filename="$1"
 cd /scr/hyoklee/src/hyrax/bes/modules/dmrpp_module/data
 ./get_dmrpp -A -b. -o /scr/hyoklee/data/$filename.dmrpp -X $filename
